9f394d9783de432dee01a7321ef1c6aa4f9eadd0
sreisner <shawn.reisner@gmail.com>
2016-03-27 04:02:25 -0500

    Create gallery directive and move into own module

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,5 @@
 (function() {
     'use strict';
 
-    var app = angular.module('printApp', []);
-
-    var galleryController = app.controller('GalleryController', ['$http', function($http) {
-        this.prints = [];
-        var gallery = this;
-
-        $http.get('/api/print').then(function(results) {
-            if(results.data.error) {
-                console.log(results.data.error);
-                return;
-            }
-            var prints = results.data;
-            prints.forEach(function(print) {
-                print.downloadFileName = print.title.substring(0, 20).split(' ').join('-') + '.stl';
-            });
-            gallery.prints = prints;
-        });
-    }]);
+    var app = angular.module('printer-app', ['gallery']);
 })();
