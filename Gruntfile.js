45c3713b8c894856314aff4f022bba21b14a04ee
sreisner <shawn.reisner@gmail.com>
2016-04-02 19:39:03 -0500

    Add build process with sample files and README
    Build process involves:
    - Running JSLint on client files
    - Transpiling client javascript ES6 files to ES5 w/ Babel/Browserify
    - Javascript minification/bundling
    - CSS minification/bundling

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+module.exports = function(grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jslint: {
+            client: {
+                src: [
+                    './site/javascript/*.js'
+                ],
+                directives: {
+                    browser: true,
+                    devel: true,
+                    es6: true,
+                    predef: ['$']
+                }
+            }
+        },
+        browserify: {
+            all: {
+                options: {
+                    transform: [
+                        ["babelify", {
+                            "presets": ['es2015']
+                        }]
+                    ]
+                },
+                files: {
+                    './build/javascript/home.js': ['./site/javascript/global.js']
+                }
+            }
+        },
+        uglify: {
+            all: {
+                expand: true,
+                cwd: './build/javascript',
+                src: ['*.js'],
+                dest: './dist/javascript/',
+                ext: '.min.js'
+            }
+        },
+        cssmin: {
+            all: {
+                files: [{
+                    expand: true,
+                    cwd: './site/css',
+                    src: ['*.css'],
+                    dest: './build/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        concat: {
+            all: {
+                files: {
+                    './dist/css/home.min.css': [
+                        './build/css/global.min.css',
+                        './build/css/gallery.min.css'
+                    ]
+                }
+            }
+        },
+        exec: {
+            cleanup: 'rm -rf ./build'
+        },
+        watch: {
+            scripts: {
+                files: ['./site/**/*'],
+                tasks: ['build']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('build', [
+        'jslint',
+        'browserify',
+        'uglify',
+        'cssmin',
+        'concat',
+        'exec:cleanup'
+    ]);
+};
