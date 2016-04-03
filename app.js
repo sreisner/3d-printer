6b7e08c31f60b82e6004bc6ce415be0508b4b18d
sreisner <shawn.reisner@gmail.com>
2016-04-03 14:00:11 -0500

    Add CORS support for all requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     var express = require('express');
     var bodyParser = require('body-parser');
     var path = require('path');
+    var cors = require('cors');
     var app = express();
 
     var routes = require('./routes');
@@ -11,6 +12,7 @@
 
     app.use(morgan('dev'));
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
 
     app.db = db.connect('mongodb://localhost:27018/printer');
     routes.initializeRoutes(app);
