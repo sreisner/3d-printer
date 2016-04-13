c97abbbb6f291f6e1b3b07f7bd71bfb38a127b4b
sreisner <shawn.reisner@gmail.com>
2016-04-13 12:45:33 -0500

    Export api hooks instead of running in index

---
+++ index.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    var morgan = require('morgan');
+    var express = require('express');
+    var bodyParser = require('body-parser');
+    var path = require('path');
+    var cors = require('cors');
+    var app = express();
+
+    var routes = require('./routes');
+    var db = require('./db');
+    var auth = require('./auth');
+
+    function addApiHooks(app, mongodb_url) {
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(cors());
+
+      app.db = db.connect(mongodb_url);
+      routes.initializeRoutes(app);
+      auth.initializeFacebookAuth(app);
+    }
+
+    module.exports = {
+      addApiHooks: addApiHooks
+    };
+})();
